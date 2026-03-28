c24f42144e777b38fc0e1f2bcba9a8ed98a49407
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2026-03-28 23:33:40 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+import { BrowserRouter } from 'react-router-dom';
 
+ReactDOM.render(
+  <BrowserRouter basename="/hw-6-1">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
