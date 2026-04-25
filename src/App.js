7e8d924238b034e86193886b8e97160f61be9f09
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2026-04-25 21:14:26 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Home from "./pages/Home/Home";
-import Movies from "./pages/Movies/Movies";
-import MovieDetails from "./pages/MovieDetails/MovieDetails";
-import Cast from "./pages/Cast/Cast";
-import Reviews from "./pages/Reviews/Reviews";
+const Home = lazy(() => import("./pages/Home/Home"));
+const Movies = lazy(() => import("./pages/Movies/Movies"));
+const MovieDetails = lazy(() => import("./pages/MovieDetails/MovieDetails"));
+const Cast = lazy(() => import("./pages/Cast/Cast"));
+const Reviews = lazy(() => import("./pages/Reviews/Reviews"));
 
 function App() {
   return (
+    <Suspense fallback={<p>Loading...</p>}>
     <Routes>
       <Route path="/" element={<Home />} />
 
@@ -19,6 +21,7 @@ function App() {
         <Route path="reviews" element={<Reviews />} />
       </Route>
     </Routes>
+    </Suspense>
   );
 }
 
